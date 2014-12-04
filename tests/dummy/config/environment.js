54a78aa1359ff0f8377ea476d5e0348aee5f6fd5
Joshua Forisha <jforisha@softlayer.com>
2014-12-04 10:39:20 -0600

    Set base URL to /ember-foundation

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/ember-foundation/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
